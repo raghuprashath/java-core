54e3ef7568331c72fc67751344d52d126b64825b
Raghu prashath <hrgamer072@gmail.con>
2024-03-05 23:16:02 +0530

    Day 3 Practice Excercises

--- MLA Phase 0 - Practice Exercises/Day3/src/com/learning/core/day3/D03P14.java
+++ MLA Phase 0 - Practice Exercises/Day3/src/com/learning/core/day3/D03P14.java
@@ -1,49 +1,46 @@
 package com.learning.core.day3;
-
-import java.util.Scanner;
-
+import java.util.*;
 public class D03P14 {
+
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the first Array Size");
-		int n=sc.nextInt();
-		int arr1[]= new int[n];
-		System.out.println("Enter the Elements");
-		for(int i=0;i<n;i++) {
-			arr1[i]=sc.nextInt();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int a[] = new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
 		}
-		System.out.println("Enter the Second Array Size");
-		int m=sc.nextInt();
-		int arr2[]= new int[n];
-		System.out.println("Enter the Elements");
+		int m = sc.nextInt();
+		int b[] = new int[m];
+		for(int i=0;i<m;i++)
+		{
+			b[i]=sc.nextInt();
+		}
+		int l =m+n;
+		int k[]=new int[l];
+		for(int i=0;i<n;i++)
+		{
+			k[i]=a[i];
+		}
 		for(int i=0;i<m;i++) {
-			arr2[i]=sc.nextInt();
+			k[i+n]=b[i];
 		}
-		Union(arr1,arr2);
-	}
-	public static void Union(int[] arr1,int[] arr2) {
-		int x=arr1.length+arr2.length+1;
-		int[] union=new int[x];
-		int count=0,i=0,j=0;
-		while(i<arr1.length && j<arr2.length) {
-			if(arr1[i]<arr2[j]) {
-				union[count]=arr1[i];
-				count++;
-				i++;
-			}
-			else if(arr1[i]>arr2[j]) {
-				union[count]=arr2[j];
-				count++;
-				j++;
-			}
-			else {
-				i++;
-				j++;
-			}
-			for(int k=0;i<x;k++) {
-				System.out.print(union[k]);
+		Arrays.sort(k);
+		System.out.print("Union : "+k[0]+" ");
+		for(int i=1;i<l;i++)
+		{
+			if(k[i]!=k[i-1]) {
+				System.out.print(k[i]+" ");
 			}
 		}
-		
+		System.out.print(" Intersection : ");
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<m;j++) {
+				if(a[i]==b[j]) {
+					System.out.print(a[i]+" ");
+				}
+			}
+		}
 	}
+
 }
