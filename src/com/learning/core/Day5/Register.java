0c8060c17bde8e9cc1ac058fcf1e0b0fd0a389a2
RAGHU PRASHATH M A <109653628+raghuprashath@users.noreply.github.com>
2024-03-08 14:39:08 +0530

    Update Register.java

--- src/com/learning/core/Day5/Register.java
+++ src/com/learning/core/Day5/Register.java
@@ -3,7 +3,8 @@ package com.learning.core.day5;
 import java.util.Scanner;
 
 public class Register <T>
-{  T obj;
+{  
+    T obj;
     String registerid;
 
    public Register(T obj) 
@@ -16,10 +17,10 @@ public String genertaeRegisterId(T obj)
 	String res=null;
 	 if(obj instanceof Employee || obj instanceof Student)
    {
-	   Scanner sc = new Scanner(System.in);
+       Scanner sc = new Scanner(System.in);
        System.out.println("enter length of random number");
        int n= sc.nextInt();
-	   String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "abcdefghijklmnopqrstuvwxyz";
+       String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "abcdefghijklmnopqrstuvwxyz";
        StringBuilder sb = new StringBuilder(n);
 
        for (int i = 0; i < n; i++) {
