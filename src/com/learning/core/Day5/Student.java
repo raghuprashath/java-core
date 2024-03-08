980fa7c7f9808f01e903f65455439f087f2a41b3
RAGHU PRASHATH M A <109653628+raghuprashath@users.noreply.github.com>
2024-03-08 14:39:33 +0530

    Update Student.java

--- src/com/learning/core/Day5/Student.java
+++ src/com/learning/core/Day5/Student.java
@@ -32,7 +32,7 @@ public class Student
    public String toString() {
 	   	return name + " " + phoneno + " " + passportNo + " " + licenseNo
 			+ " " + panCardNo + " " + voterid + " " + getClass() + " "
-			+ hashCode() + " " + super.toString() + "]";
+			+ hashCode() + " " + super.toString();
    }	
 
 }
